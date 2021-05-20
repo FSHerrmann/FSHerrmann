193c2e08b31a2b8d31310106fad62e2a3e3b9a16
Felipe Schneider Herrmann <mime.schneider@gmail.com>
2021-05-20 11:18:51 -0300

    Add files via upload
    Arquivos até 20/05/21

---
+++ Python/Cursos Python/Python para Android, iOS, Win, Linux e Mac - Desde o Básico/Instrução_global.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 16 10:17:06 2021
+
+@author: Felipe Schneider Herrmann
+"""
+
+# Global Scope
+num = 10
+print(num)
+
+
+def func():
+    print(num*2)
+
+
+print(func())
